3577473ca03049aa8b13cf82351e68d96a070512
MatRos-sf <mateusz-rosenkranz@wp.pl>
2022-05-23 14:56:43 +0200

    edit home page

--- puzzle/templatetags/simple_extra_tag.py
+++ puzzle/templatetags/simple_extra_tag.py
@@ -1,5 +1,6 @@
 from django import template
 from puzzle.models import Company, Puzzle
+from django.db.models import Count
 
 
 register = template.Library()
@@ -9,9 +10,18 @@ def number_company():
     return Company.objects.count()
 
 @register.simple_tag
+def company_with_more_puzzles(count=3):
+    return Company.objects.annotate(
+        total_puzzles = Count('puzzles')
+    ).order_by('-total_puzzles')[:count]
+
+
+@register.simple_tag
 def get_most_popular_puzzle(count=5):
     """
     Później z listy plubień trzeba  zabrać
 
     """
     return Puzzle.objects.all()[:count]
+
+#pierwsze 100 znaków
